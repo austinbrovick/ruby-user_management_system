a9ba2f185b85b07fd052fe1e9df065a5bad17383
austinbrovick <austinbrovick@gmail.com>
2016-04-09 16:07:31 -0700

    commit with the ability to add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
     puts "I have made it to the index"
     @users = User.all
   end
+  def new
+
+  end
+  def create
+    @info = params[:info]
+
+    if @info[:first_name].length > 3 and @info[:last_name].length > 3 and @info[:email].length > 3 and @info[:password].length > 3
+      puts @info
+      puts "$" * 100
+      User.create(first_name:@info[:first_name], last_name:@info[:last_name], email:@info[:email], password:@info[:password])
+    elsif
+      puts "you got to give me more!!"
+    end
+    puts "hello from create function   " + "$$" * 20
+    redirect_to '/users'
+  end
 end
